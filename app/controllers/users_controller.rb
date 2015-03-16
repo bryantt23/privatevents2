30a0a30c8eca14f5caa9a6083042db428a485585
bryantt23 <bryantt23@gmail.com>
2015-03-16 01:28:37 -0700

    add sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	include SessionsHelper
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "You have created a new User."
+			sign_in(@user)
+		else
+			flash.now[:danger] = "Invalid parameters."
+			render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+end
